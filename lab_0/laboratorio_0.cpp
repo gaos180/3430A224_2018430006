dfbdffefb20f85e7e753b7205294d3eafe6a42f0
Gaos <gabriel.rhormazabal@gmail.com>
2024-08-11 17:04:33 -0400

    se ha añadido mejor sintaxis ademas de lo pedido del 1 al 10

--- lab_0/laboratorio_0.cpp
+++ lab_0/laboratorio_0.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-// en caso de que se permita
+// En caso de que se permita 
 #include <string>
 // Funciones print creadas debido a que no quería escribir cout[...] ;
 void print_char(char *charToPrint)
@@ -18,11 +18,34 @@ void print_int(int numberToPrint)
 
 void even_or_odd(int number)
 {
+    std:: cout << "El número: " << number;
     if (number % 2 == 0){
-        print_char("Es par");
+        print_char(" es par\n");
     } else {
-        print_char("Es impar");
+        print_char(" es impar\n");
     }
+}
+
+void even_or_odd_1_10(){
+    for (int i = 1; i <= 10; i++)
+    {   
+        even_or_odd(i);
+    }
+    
+}
+
+void working_with_word(){
+   // si deja usar string sirve de la siguiente forma
+
+    std::string word;
+
+    print_char("Ingrese una cadena de caracteres:");
+    std:: cin >> word;
+
+    std:: cout << word;
+    std:: cout << "\nEl tamaño de la pabra es de:"<< word.length();
+
+ 
 }
 
 int main()
@@ -36,16 +59,37 @@ int main()
     print_int(number);
 
     even_or_odd(number);
+    
+    even_or_odd_1_10();
 
-    // si deja usar string sirve de la siguiente forma
+    // Otras formas:
+    /*
+    print_char("El número 1 es impar");
+    print_char("El número 2 es par");
+    print_char("El número 3 es impar");
+    print_char("El número 4 es par");
+    print_char("El número 5 es impar");
+    print_char("El número 6 es par");
+    print_char("El número 7 es impar");
+    print_char("El número 8 es par");
+    print_char("El número 9 es impar");
+    print_char("El número 10 es par");
+    */
+   // o de la siguiente forma:
+   /*
+    even_or_odd(1);
+    even_or_odd(2);
+    even_or_odd(3);
+    even_or_odd(4);
+    even_or_odd(5);
+    even_or_odd(6);
+    even_or_odd(7);
+    even_or_odd(8);
+    even_or_odd(9);
+    even_or_odd(10);
+   
+   */
 
-    std::string word;
-
-    print_char("Ingrese una cadena de caracteres:");
-    std:: cin >> word;
-
-    std:: cout << word;
-    std:: cout << "\nEl tamaño de la pabra es de:"<< word.length();
-
+    working_with_word();   
     return 0;
 }
